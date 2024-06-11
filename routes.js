16aced51a8f2955da975155c99d1859bf70729f0
w1y0n0 <wiyono30@gmail.com>
2024-06-11 15:17:13 +0700

    Body/Payload Request

--- routes.js
+++ routes.js
@@ -42,6 +42,14 @@ const routes = [
         },
     },
     {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}!`;
+        },
+    },
+    {
         method: '*',
         path: '/{any*}',
         handler: (request, h) => {
