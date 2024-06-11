00ca0fb06defbac6a57a1fc039cce8cfc3a94fdd
w1y0n0 <wiyono30@gmail.com>
2024-06-11 10:18:17 +0700

    Method/Verb Request dan Routing

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 // console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
+
     const server = Hapi.server({
         port: 5000,
         host: 'localhost',
     });
+
+    // server.route({
+    //     method: 'GET',
+    //     path: '/',
+    //     handler: (request, h) => {
+
+    //         return 'Hello World!';
+    //     }
+    // });
+
+    // server.route([
+    //     {
+    //         method: 'GET',
+    //         path: '/',
+    //         handler: (request, h) => {
+    //             return 'Homepage';
+    //         },
+    //     },
+    //     {
+    //         method: 'GET',
+    //         path: '/about',
+    //         handler: (request, h) => {
+    //             return 'About Page';
+    //         },
+    //     },
+    // ]);
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
