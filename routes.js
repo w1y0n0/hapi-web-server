4bd4ee7b7556c7c983c38c8b0ed986e60b4fc709
w1y0n0 <wiyono30@gmail.com>
2024-06-14 07:57:12 +0700

    Response Toolkit

--- routes.js
+++ routes.js
@@ -50,6 +50,13 @@ const routes = [
         },
     },
     {
+        method: 'POST',
+        path: '/user',
+        handler: (request, h) => {
+            return h.response('created').code(201);
+        },
+    },
+    {
         method: '*',
         path: '/{any*}',
         handler: (request, h) => {
@@ -57,5 +64,20 @@ const routes = [
         },
     },
 ];
+
+// Detailed notation
+// const handler = (request, h) => {
+//     const response = h.response('success');
+//     response.type('text/plain');
+//     response.header('Custom-Header', 'some-value');
+//     return response;
+// };
+
+// Chained notation
+const handler = (request, h) => {
+    return h.response('success')
+        .type('text/plain')
+        .header('Custom-Header', 'some-value');
+};
 
 module.exports = routes;
